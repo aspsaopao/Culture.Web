df5eaf389c1fb6fa5343b0638d56beddcd0dac7a
oncejob 杨 <778926391@qq.com>
2021-04-24 23:04:23 +0800

    保存更新

--- Tourism.Web/ClientApp/src/app/listen/listen.component.ts
+++ Tourism.Web/ClientApp/src/app/listen/listen.component.ts
@@ -15,8 +15,26 @@ export class ListenComponent {
 
     }
     list: any[] = [1, 2, 3, 4];
-
+    userName = localStorage.getItem('userInfo')?localStorage.getItem('userInfo'):"登录"
+    /**查看详情 */
+    goDetails() {
+        this.route.navigate(['details']);
+    }
     go() {
         this.route.navigate(['index/details']);
+    }
+    login() {
+        if(this.userName!=="登录"){
+            return;
+        }else {
+            this.route.navigate(['app/login']);
+        }
+    }
+    goPersonalCenter() {
+        this.route.navigate(['app/personalcenter']);
+    }
+    output() {
+        localStorage.removeItem('userInfo');
+        this.route.navigate(['app/login']);
     }
 }
